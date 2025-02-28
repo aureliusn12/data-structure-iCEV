d05d7ecbca2dfe9205d41f0bc37702ca65c00b5e
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2025-02-27 21:24:04 -0300

    first exercise

---
+++ First class/StaticList.java
@@ -0,0 +1,57 @@
+public class StaticList {
+    private int valor[];
+    private int tamanho, ultimo;
+
+        public StaticList(int p){
+            this.tamanho = p;
+            this.valor = new int[p];
+            this.ultimo = - 1;
+        }
+    
+        public boolean add (int elemento, int p){
+            if (p < 0 || p > ultimo + 1 || ultimo + 1 >= tamanho) {
+                return false;
+            }
+    
+            for (int i = ultimo; i >= p; i--){
+                valor [i + 1] = valor[i];
+            }
+            valor[p] = elemento;
+            ultimo++;
+            return true;
+        }
+        public boolean remove(int p){
+            if (p < 0 || p > ultimo){
+                return false;
+            }
+    
+            for (int i = p; i < ultimo; i++){
+                valor[i] = valor [i + 1];
+            }
+    
+            ultimo--;
+            return true;
+        }
+         
+        public void imprimir(){
+            for(int i = 0; i <= ultimo; i++){
+                System.out.println(valor[i] + " ");
+            }
+            System.out.println();
+        }
+    
+        public static void main(String[] args) {
+            StaticList lista = new StaticList(10);
+
+        lista.add(5,0);
+        lista.add(10,1);
+        lista.add(15,2);
+        lista.add(20,1);
+        
+        lista.imprimir();
+
+        lista.remove(2);
+
+        lista.imprimir();
+    }
+}
